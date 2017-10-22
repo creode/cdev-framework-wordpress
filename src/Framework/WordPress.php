02b39c8662455e5bb9b0bb44d6d939d3f11d74a9
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-10-22 13:29:21 +0100

    Initial commit

---
+++ src/Framework/WordPress.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cdev\WordPress\Framework;
+
+use Creode\Framework\Framework;
+
+class WordPress extends Framework
+{
+    const NAME = 'wordpress';
+    const LABEL = 'WordPress';
+
+    /**
+     * Returns commands to clear cache on this framework
+     * @return array
+     */
+    public function clearCache()
+    {
+        return [];
+    }
+
+    /**
+     * Returns commands to run updates on this framework
+     * @return array
+     */
+    public function update()
+    {
+        return [];
+    }
+}
